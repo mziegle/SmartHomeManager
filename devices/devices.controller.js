ab2a6e479d617a1c58303d5679aa8a7794dc2a6c
Michael Ziegler <ziegler0@hm.edu>
2018-10-07 15:56:47 +0200

    implements the devices view

--- devices/devices.controller.js
+++ devices/devices.controller.js
@@ -1,6 +1,24 @@
 SmartHomeManger
 
-    .controller('devicesController', ['$route', function () {
-        console.log('devicesController loaded')
+    .controller('devicesController', ['$route', function() {
+
+        var devicesController = this;
+
+        devicesController.devices = [
+            {
+                id: 0,
+                name: "Camera Backyard",
+                ip: "10.10.10.1",
+                status: "off"
+            },
+            {
+                id: 1,
+                name: "Heater",
+                ip: "10.10.10.2",
+                status: "off"
+            }
+        ];
+
+        console.log(devicesController.devices);
     }]);
     
